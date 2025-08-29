2676209435b7317d2c7c74eff55089459e51a6d0
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-08-29 17:32:09 -0300

    chore(lib): add prettier, husky, eslint and commitlint configs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,6 +2,9 @@ import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
 
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import pluginPrettier from "eslint-plugin-prettier";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
@@ -9,17 +12,27 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [
+export default [
+  // Configurações padrão do Next.js
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Regras personalizadas
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    files: ["**/*.{js,jsx,ts,tsx}"],
+    plugins: {
+      "react-hooks": pluginReactHooks,
+      prettier: pluginPrettier,
+    },
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "prettier/prettier": "error",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
   },
 ];
 
-export default eslintConfig;
