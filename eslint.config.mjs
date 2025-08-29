1f2f20adeb2348167553ccf9d77595f665e6d8f7
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-08-29 17:50:43 -0300

    chore: add simple import sort config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import { FlatCompat } from "@eslint/eslintrc";
 
 import pluginReactHooks from "eslint-plugin-react-hooks";
 import pluginPrettier from "eslint-plugin-prettier";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -22,11 +23,14 @@ export default [
     plugins: {
       "react-hooks": pluginReactHooks,
       prettier: pluginPrettier,
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       "react-hooks/rules-of-hooks": "error",
       "react-hooks/exhaustive-deps": "warn",
       "prettier/prettier": "error",
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
     },
     settings: {
       react: {
@@ -35,4 +39,3 @@ export default [
     },
   },
 ];
-
--- src/app/(public)/_components/header/header.tsx
+++ src/app/(public)/_components/header/header.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+
 import { SheetButton } from './sheet-button';
 
 const Header = () => {
