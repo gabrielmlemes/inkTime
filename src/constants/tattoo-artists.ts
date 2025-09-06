2d6e7376c4fea344f604eaaaaa6cae9b2a687d38
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-09-06 19:28:37 -0300

    chore(cards): create card component

--- src/constants/tattoo-artists.ts
+++ src/constants/tattoo-artists.ts
@@ -8,17 +8,16 @@ import tatuadora6 from '@/assets/tattoo-artists/tatuadora6.png';
 import tatuadora9 from '@/assets/tattoo-artists/tatuadora9.png';
 
 export const tattooArtists = [
-  { id: 1, name: 'Bruno Costa', style: 'Realismo', image: tatuador3 },
-  { id: 2, name: 'Alice Silva', style: 'Old School', image: tatuadora2 },
-  { id: 3, name: 'Diego Rocha', style: 'Aquarela', image: tatuador4 },
-  { id: 4, name: 'Carla Mendes', style: 'Geométrico', image: tatuadora5 },
-  { id: 5, name: 'Elena Ferreira', style: 'Minimalista', image: tatuadora6 },
-  { id: 6, name: 'Fábio Lima', style: 'Tribal', image: tatuador7 },
+  { name: 'Bruno Costa', style: 'Realismo', image: tatuador3 },
+  { name: 'Alice Silva', style: 'Old School', image: tatuadora2 },
+  { name: 'Diego Rocha', style: 'Aquarela', image: tatuador4 },
+  { name: 'Carla Mendes', style: 'Geométrico', image: tatuadora5 },
+  { name: 'Elena Ferreira', style: 'Minimalista', image: tatuadora6 },
+  { name: 'Fábio Lima', style: 'Tribal', image: tatuador7 },
   {
-    id: 7,
     name: 'Gabriela Santos',
     style: 'Neo Tradicional',
     image: tatuadora9,
   },
-  { id: 8, name: 'Hugo Almeida', style: 'Blackwork', image: tatuador8 },
+  { name: 'Hugo Almeida', style: 'Blackwork', image: tatuador8 },
 ];
