3de1ae6f681e1162cb4a070941c36fa2e7d570cf
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-09-12 15:03:45 -0300

    feat(auth): implement NextAuth with Prisma adapter and GitHub provider

---
+++ src/lib/auth.ts
@@ -0,0 +1,12 @@
+import { PrismaAdapter } from '@auth/prisma-adapter';
+import NextAuth from 'next-auth';
+import { Adapter } from 'next-auth/adapters';
+import GitHub from 'next-auth/providers/github';
+
+import prisma from './prisma';
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma) as Adapter,
+  trustHost: true,
+  providers: [GitHub],
+});
