090213768c8dcf8c5dd9bda5319a9cdae1714e26
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-09-16 20:55:30 -0300

    feat(auth): create getServerSession utility for session handling

--- src/app/(private)/dashboard/page.tsx
+++ src/app/(private)/dashboard/page.tsx
@@ -1,4 +1,14 @@
-const Dashboard = () => {
+import { redirect } from 'next/navigation';
+
+import getServerSession from '@/lib/get-server-session';
+
+const Dashboard = async () => {
+  const session = await getServerSession();
+
+  if (!session) {
+    return redirect('/login');
+  }
+
   return <div>Dashboard Page</div>;
 };
 
