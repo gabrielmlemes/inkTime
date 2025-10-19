2b0296e73f20e2fd5033b58e23adf3298d30fc71
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-10-19 17:45:54 -0300

    refactor(profile): refactor useProfileForm to accept user profile data as props

--- src/app/(private)/dashboard/profile/hooks/profile-form.tsx
+++ src/app/(private)/dashboard/profile/hooks/profile-form.tsx
@@ -3,18 +3,31 @@ import { useForm } from 'react-hook-form';
 
 import { ProfileFormData, profileSchema } from '../schemas/profile-schema';
 
-const useProfileForm = () => {
-  return useForm<ProfileFormData>({
+interface UserProfileFormProps {
+  name: string | null;
+  address: string | null;
+  phone: string | null;
+  status: boolean;
+  timezone: string | null;
+}
+
+export default function useProfileForm({
+  name,
+  address,
+  phone,
+  status,
+  timezone,
+}: UserProfileFormProps) {
+  const form = useForm<ProfileFormData>({
     resolver: zodResolver(profileSchema),
+    mode: 'onBlur',
     defaultValues: {
-      name: '',
-      address: '',
-      phone: '',
-      status: false,
-      times: [],
-      timezone: '',
+      name: name || '',
+      address: address || '',
+      phone: phone || '',
+      status: status || false,
+      timezone: timezone || '',
     },
   });
-};
-
-export default useProfileForm;
+  return form;
+}
