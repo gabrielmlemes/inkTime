bc70bd317b032d179e526defc6b633b8b6b6752e
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-10-20 16:44:01 -0300

    feat(profile): update profile page to use ProfileForm component

--- src/app/(private)/dashboard/profile/page.tsx
+++ src/app/(private)/dashboard/profile/page.tsx
@@ -1,7 +1,15 @@
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Meu perfil - Dashboard',
+  description: 'Gerencie as informações do seu perfil.',
+};
+
 import { redirect } from 'next/navigation';
 
 import getServerSession from '@/lib/get-server-session';
 
+import ProfileForm from './_components/profile-form';
 import { getUserInfo } from './_data-access/get-user-info';
 
 const Profile = async () => {
@@ -17,7 +25,7 @@ const Profile = async () => {
     redirect('/login');
   }
 
-  return <div>Profile Page</div>;
+  return <ProfileForm user={user} />;
 };
 
 export default Profile;
