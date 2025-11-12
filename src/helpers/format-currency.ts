5a2de5868fa44eb1b0e90637755d96c222f1d3c7
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-11-12 20:35:47 -0300

    feat(helper): create formatCurrency helper

---
+++ src/helpers/format-currency.ts
@@ -0,0 +1,9 @@
+// Format a number (in cents) to Brazilian Real currency format
+export function formatCurrency(value: number): string {
+  return new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  }).format(value / 100);
+}
