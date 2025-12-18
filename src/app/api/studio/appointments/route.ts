99d425e4c6ea3c27ec2e610bd5a66fb008b3b02b
Gabriel Lemes <g.moreiralemess@gmail.com>
2025-12-18 16:44:15 -0300

    feat(route): implement protected route to fetch studio appointments

---
+++ src/app/api/studio/appointments/route.ts
@@ -0,0 +1,48 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+import { auth } from '@/lib/auth';
+import prisma from '@/lib/prisma';
+
+// Rota protegida para buscar agendamentos do estúdio (só acessível se estiver autenticado)
+export const GET = auth(async function GET(request) {
+  if (!request.auth) {
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
+  }
+
+  const searchParams = request.nextUrl.searchParams;
+  const dateParams = searchParams.get('date') as string;
+
+  if (!dateParams) {
+    return NextResponse.json({ error: 'Data inválida' }, { status: 400 });
+  }
+
+  const studioId = request.auth?.user?.id;
+
+  if (!studioId) {
+    return NextResponse.json({ error: 'ID do estúdio não encontrado' }, { status: 400 });
+  }
+
+  try {
+    const [year, month, day] = dateParams.split('-').map(Number); // Converte a data recebida em um objeto Date para manipulação posterior
+    const startDate = new Date(year, month - 1, day, 0, 0, 0, 0); // Início do dia
+    const endDate = new Date(year, month - 1, day, 23, 59, 59, 999); // Fim do dia
+
+    const appointments = await prisma.appointment.findMany({
+      where: {
+        userId: studioId,
+        date: {
+          gte: startDate,
+          lte: endDate,
+        },
+      },
+      include: {
+        service: true,
+      },
+    });
+
+    return NextResponse.json({ appointments }, { status: 200 });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ error: 'Erro ao buscar agendamentos' }, { status: 400 });
+  }
+});
