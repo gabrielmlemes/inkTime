223a887f882dd3a0170b6a2c7b69ce33fa3c7ac1
Gabriel Lemes <g.moreiralemess@gmail.com>
2026-01-21 17:47:02 -0300

    feat(metadata): enhance site metadata for SEO and social sharing, add robots and sitemap functionality

--- src/app/(public)/estudio/[slug]/page.tsx
+++ src/app/(public)/estudio/[slug]/page.tsx
@@ -4,10 +4,69 @@ import { redirect } from 'next/navigation';
 import ScheduleContent from './_components/schedule-content';
 import { getStudioInfo } from './_data-access/get-estudio-info';
 
-export const metadata: Metadata = {
-  title: 'Agendamento - Inkore',
-  description: 'Agendamento facilitado para você!',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ slug: string }>;
+}): Promise<Metadata> {
+  const { slug } = await params;
+  const user = await getStudioInfo({ slug: slug });
+
+  return {
+    title: `${user?.name} | Agendamento online`,
+    description: 'Faça seu agendamento',
+    keywords: [
+      'Inkore',
+      'agendamento online',
+      'gestão de agenda',
+      'sistema de agendamento',
+      'plataforma de reservas',
+      'tatuagem',
+      'tattoo',
+      'estúdio de tatuagem',
+      'beleza',
+      'estética',
+    ],
+    icons: {
+      icon: '/favicon.svg',
+    },
+    openGraph: {
+      title: `${user?.name} | Agendamento online`,
+      description: 'Faça seu agendamento',
+      type: 'website',
+      url: `https://www.inkorestudio.com.br/${user?.slug}`,
+      siteName: 'Inkore',
+      images: [
+        {
+          url: `https://www.inkorestudio.com.br/${user?.image}`,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: `${user?.name} | Agendamento online`,
+      description: 'Faça seu agendamento',
+      images: [`https://www.inkorestudio.com.br/${user?.image}`],
+    },
+    alternates: {
+      canonical: `https://www.inkorestudio.com.br/${user?.slug}`,
+    },
+    robots: {
+      index: true,
+      follow: true,
+      nocache: true,
+      googleBot: {
+        index: true,
+        follow: true,
+        noimageindex: true,
+        'max-video-preview': -1,
+        'max-image-preview': 'large',
+        'max-snippet': -1,
+      },
+    },
+    metadataBase: new URL(`https://www.inkorestudio.com.br/${user?.slug}`),
+  };
+}
 
 export default async function SchedulePage({ params }: { params: Promise<{ slug: string }> }) {
   const { slug } = await params;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,11 +18,61 @@ const geistMono = Geist_Mono({
 });
 
 export const metadata: Metadata = {
-  title: 'Inkore',
-  description: 'A sua plataforma de agendamentos online.',
+  title: 'Inkore - Plataforma de Agendamento Online',
+  description:
+    'Gestão inteligente de agendamentos para estúdios de tatuagem. Otimize sua rotina e foque na sua arte.',
+  keywords: [
+    'Inkore',
+    'agendamento online',
+    'gestão de agenda',
+    'sistema de agendamento',
+    'plataforma de reservas',
+    'tatuagem',
+    'tattoo',
+    'estúdio de tatuagem',
+    'beleza',
+    'estética',
+  ],
   icons: {
     icon: '/favicon.svg',
   },
+  openGraph: {
+    title: 'Inkore - Plataforma de Agendamento Online',
+    description:
+      'Gestão inteligente de agendamentos para estúdios de tatuagem. Otimize sua rotina e foque na sua arte.',
+    type: 'website',
+    url: 'https://www.inkorestudio.com.br',
+    siteName: 'Inkore',
+    images: [
+      {
+        url: 'https://www.inkorestudio.com.br/landingExample.jpg',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Inkore',
+    description:
+      'Gestão inteligente de agendamentos para estúdios de tatuagem. Otimize sua rotina e foque na sua arte.',
+    images: ['https://www.inkorestudio.com.br/landingExample.jpg'],
+  },
+  alternates: {
+    canonical: 'https://www.inkorestudio.com.br',
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      noimageindex: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
+  metadataBase: new URL('https://www.inkorestudio.com.br'),
 };
 
 export default function RootLayout({
